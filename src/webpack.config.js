ef10c9af46acb41b31d4260ccfaf0f48754a5d13
Hammad <hammad4299@gmail>
2018-08-26 20:33:39 +0500

    no message

--- src/webpack.config.js
+++
@@ -1,7 +0,0 @@
-//#add following externals
-{
-    'externals/CSRFToken': 'csrftoken', //now import xyz from 'myExternal' should work
-    'externals/Routes': 'routes',
-    'externals/SiteConfig': 'siteConfig',
-    'jquery':'jQuery'
-}
---
+++ src/webpack/webpack-project.ts
@@ -0,0 +1,8 @@
+const projectConfig:IProjectSettings = {
+    externals: {
+		'externals/CSRFToken': 'csrftoken', //now import xyz from 'myExternal' should work
+		'externals/Routes': 'routes',
+		'externals/SiteConfig': 'siteConfig',
+		'jquery':'jQuery'
+	}
+};
