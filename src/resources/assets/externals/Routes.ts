a23d31694d00cb1d75841caa523d2a3a02d92ed4
Hammad <hammad4299@gmail>
2018-09-06 23:13:05 +0500

    no message

--- src/resources/assets/externals/Routes.ts
+++
@@ -1,4 +0,0 @@
-const a:any = {
-    baseurl: ''
-};
-export default a;
--- src/webpack-project.ts
+++ src/webpack-project.ts
@@ -1,7 +1,7 @@
 const projectConfig:IProjectSettings = {
     externals: {
 		'externals/CSRFToken': 'csrftoken', //now import xyz from 'myExternal' should work
-		'externals/Routes': 'routes',
+		'externals/RemoteRoutes': 'RemoteRoutes',
 		'externals/SiteConfig': 'siteConfig',
 		'jquery':'jQuery'
 	}
